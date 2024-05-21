9c3585595fb86d0184175e312d28e6b32bb5e26c
Denis McMahon <denismcmahon@gmail.com>
2024-05-21 13:59:59 +0100

    basic version of add post form added

--- src/app/pages/add-post/add-post.component.ts
+++ src/app/pages/add-post/add-post.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.scss']
 })
-export class AddPostComponent implements OnInit {
+export class AddPostComponent {
+  title: string = '';
+  content: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
+  onSubmit() {
+    const post = { title: this.title, content: this.content }
+    this.http.post('http://localhost:3000/api/posts', post).subscribe(response => {
+      console.log('Post saved', response);
+    })
   }
-
 }
