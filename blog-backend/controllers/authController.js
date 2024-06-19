7985ea408bfbc75a85564a6a6ea722cf0ea20b70
Denis McMahon <denismcmahon@gmail.com>
2024-06-19 13:02:06 +0100

    starting route/controller seperation

---
+++ blog-backend/controllers/authController.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const login = async (req, res) => {
+  console.log('DM login endpoint called');
+  const { username, password } = req.body;
+  console.log('username and password', username, password);
+  try {
+    const user = await User.findOne({ username });
+    console.log('user: ', user);
+    if (!user) {
+      return res.status(401).send('Invalid credentials');
+    }
+    console.log('DM ==> login route ==> password: ', password);
+    console.log('DM ==> login route ==> user.password: ', user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
+    console.log('DM ==> login route ==> isMatch: ', isMatch);
+    if (!isMatch) {
+      return res.status(401).send('Invalid credentials');
+    }
+    console.log('DM ==> login route ==> setting up token');
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+      expiresIn: '1h'
+    });
+    console.log('DM ==> login route ==> token: ', token);
+    res.json({ token });
+  } catch (error) {
+    console.error('DM ==> login route error: ', error);
+    res.status(500).send('An error occurred');
+  }
+}
+
+module.exports = {
+  login
+};
