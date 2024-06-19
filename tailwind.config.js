5b73b7f398885a9cfa3dae3511874ba6dc258893
Denis McMahon <denismcmahon@gmail.com>
2024-06-19 10:57:46 +0100

    color scheme colors added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#009688',
+        secondary: '#FF6F61',
+        background: '#F5F5F5',
+        textPrimary: '#333333',
+        testSecondary: '#757575',
+        buttonBg: '#009688',
+        buttonText: '#FFFFFF',
+        navbarBg: '#E0F2F1',
+        navbarText: '#009688',
+      }
+    },
   },
   plugins: [],
 }
